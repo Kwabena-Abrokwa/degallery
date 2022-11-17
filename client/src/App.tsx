44a6fb42c1cb899a029129874a87cc3b3ec08e8e
Frank Kwabena Abrokwa <76174290+Kwabena-Abrokwa@users.noreply.github.com>
2022-11-17 14:30:25 +0000

    Updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,34 +2,19 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPages from "./interface/Pages/Guests/LandingPages";
-import { useAppDispatch, useAppSelector } from "./logic/ReduxStore/app/hooks";
-import { toggleWindowLoader } from "./logic/ReduxStore/feature/GlobalStates/GlobalStateSlice";
 import Home from "./interface/Pages/Dashboard/Home";
 
 interface AppProps {}
 
 const App: React.FC<AppProps> = ({}) => {
-	const dispatch = useAppDispatch();
-	const windowLoader = useAppSelector(
-		(state) => state.getGlobalStates.windowLoader
-	);
-
-	const loader = () => {
-		dispatch(toggleWindowLoader(false));
-	};
-
-	window.onload = loader;
-
 	return (
 		<>
-			{windowLoader ? (
-				<></>
-			) : (
-				<Routes>
-					<Route path="/" element={<LandingPages />} />
-					<Route path="/dashboard" element={<Home />} />
-				</Routes>
-			)}
+			: (
+			<Routes>
+				<Route path="/" element={<LandingPages />} />
+				<Route path="/dashboard" element={<Home />} />
+			</Routes>
+			)
 		</>
 	);
 };
